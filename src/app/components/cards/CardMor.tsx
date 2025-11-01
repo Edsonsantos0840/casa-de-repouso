967f68eba4f0824b4f9be5a2d5e0c98810cf4881
Edson Santos <edsonpsantos0840@gmail.com>
2025-11-01 21:49:58 +0000

    ajustes no card mor

--- src/app/components/cards/CardMor.tsx
+++ src/app/components/cards/CardMor.tsx
@@ -6,14 +6,14 @@ export default function CardMor({sty}: {sty?: string}) {
     <Link
       href="https://wa.me/5519987671072?text=Ol%C3%A1%2C%20Estava%20vendo%20seu%20site%20e%20gostaria%20de%20saber%20mais."
       target="_blank"
-      className="w-36 lg:px-3 py-3 bg-principal2 rounded-lg font-semibold hover:bg-textoPrincipal hover:text-principal transition"
+      className="w-40 lg:px-3 py-3 bg-principal2 rounded-lg font-semibold hover:bg-textoPrincipal hover:text-principal transition"
     >
       Entre em Contato.
     </Link>
     <Link
       href="https://wa.me/5519987671072?text=Ol%C3%A1%2C%20Estava%20vendo%20seu%20site%20e%20gostaria%20de%20saber%20mais."
       target="_blank"
-      className="w-36 py-3 bg-principal2 rounded-lg font-semibold hover:bg-textoPrincipal hover:text-principal transition"
+      className="w-40 py-3 bg-principal2 rounded-lg font-semibold hover:bg-textoPrincipal hover:text-principal transition"
     >
       Saiba mais
     </Link>
