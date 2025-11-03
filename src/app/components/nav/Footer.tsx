f77a019e449595d473e8960665cde9a51cd5fe74
Edson Santos <edsonpsantos0840@gmail.com>
2025-11-03 22:13:05 +0000

    coreeção 2

--- src/app/components/nav/Footer.tsx
+++ src/app/components/nav/Footer.tsx
@@ -5,7 +5,7 @@ import CardContactSociais from "../cards/CardContactSociais";
 export default function Footer() {
   return (
     <footer className="relative inset-0 bg-gradient-to-l from-[#091b22] via-[#002b3b] to-[#091b22] text-white py-2 px-6 lg:px-2 animate-fadeIn">
-      <div className="relative container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[39%_28%_33%] gap-4 lg:gap-2  text-left lg:text-center md:items-center">
+      <div className="relative mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[39%_28%_33%] gap-4 lg:gap-2  text-left lg:text-center md:items-center">
         {/* Seção de Endereço */}
         <CardContactAdress />
 
